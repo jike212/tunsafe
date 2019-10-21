d68288c27e3f5ec1491675e34acc82e9a8145c59
jike212 <fegul@fegul.com>
2019-10-21 08:35:34 +0300

    Change ports

--- tunsafe_install.sh
+++ tunsafe_install.sh
@@ -61,7 +61,7 @@ then
 sudo cat > /etc/tunsafe/TunSafe.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.0.0.1/24 
+Address = 10.0.5.1/24 
 ObfuscateKey = $obfsstr
 PostUp   = iptables -A FORWARD -i tun0 -j ACCEPT; iptables -A FORWARD -o tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
 PostDown = iptables -D FORWARD -i tun0 -j ACCEPT; iptables -D FORWARD -o tun0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
@@ -71,14 +71,14 @@ MTU = 1380
 
 [Peer]
 PublicKey = $c2
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.0.5.2/32
 EOF
 
 
 sudo cat > /etc/tunsafe/client.conf <<-EOF
 [Interface]
 PrivateKey = $c1
-Address = 10.0.0.2/24 
+Address = 10.0.5.2/24 
 ObfuscateKey = $obfsstr
 DNS = 8.8.8.8
 MTU = 1380
@@ -96,7 +96,7 @@ then
 sudo cat > /etc/tunsafe/TunSafe.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.0.0.1/24 
+Address = 10.0.5.1/24 
 ObfuscateKey = $obfsstr
 ListenPortTCP = $port
 PostUp   = iptables -A FORWARD -i tun0 -j ACCEPT; iptables -A FORWARD -o tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
@@ -106,14 +106,14 @@ MTU = 1380
 
 [Peer]
 PublicKey = $c2
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.0.5.2/32
 EOF
 
 
 sudo cat > /etc/tunsafe/client.conf <<-EOF
 [Interface]
 PrivateKey = $c1
-Address = 10.0.0.2/24 
+Address = 10.0.5.2/24 
 ObfuscateKey = $obfsstr
 DNS = 8.8.8.8
 MTU = 1380
@@ -131,7 +131,7 @@ then
 sudo cat > /etc/tunsafe/TunSafe.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.0.0.1/24 
+Address = 10.0.5.1/24 
 ObfuscateKey = $obfsstr
 ListenPortTCP = 443
 ObfuscateTCP=tls-chrome
@@ -143,14 +143,14 @@ MTU = 1380
 
 [Peer]
 PublicKey = $c2
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.0.5.2/32
 EOF
 
 
 sudo cat > /etc/tunsafe/client.conf <<-EOF
 [Interface]
 PrivateKey = $c1
-Address = 10.0.0.2/24 
+Address = 10.0.5.2/24 
 ObfuscateKey = $obfsstr
 ObfuscateTCP=tls-chrome
 DNS = 8.8.8.8
@@ -201,14 +201,14 @@ add_user(){
     ipnum=$(grep Allowed /etc/tunsafe/TunSafe.conf | tail -1 | awk -F '[ ./]' '{print $6}')
     newnum=$((10#${ipnum}+1))
     sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
-    sed -i 's%^Address.*$%'"Address = 10.0.0.$newnum\/24"'%' $newname.conf
+    sed -i 's%^Address.*$%'"Address = 10.0.5.$newnum\/24"'%' $newname.conf
 
 cat >> /etc/tunsafe/TunSafe.conf <<-EOF
 [Peer]
 PublicKey = $(cat tempubkey)
-AllowedIPs = 10.0.0.$newnum/32
+AllowedIPs = 10.0.5.$newnum/32
 EOF
-    tunsafe set tun0 peer $(cat tempubkey) allowed-ips 10.0.0.$newnum/32
+    tunsafe set tun0 peer $(cat tempubkey) allowed-ips 10.0.5.$newnum/32
     green "添加完成，文件：/etc/tunsafe/$newname.conf"
     rm -f temprikey tempubkey
 }
